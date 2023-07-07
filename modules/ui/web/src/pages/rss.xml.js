50cc97f3744e61aaf637df8d24733afe7c189c88
Yoshimasa Tanabe <yoshimasa_tanabe@funds.jp>
2023-07-08 02:51:17 +0900

    Add prettier

--- modules/ui/web/src/pages/rss.xml.js
+++ modules/ui/web/src/pages/rss.xml.js
@@ -1,18 +1,18 @@
-import rss, {pagesGlobToRssItems} from "@astrojs/rss";
-import {getCollection} from "astro:content";
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function get() {
-    const posts = await getCollection("posts");
-    return rss({
-        title: "Astro Lerner | Blog",
-        description: "My journey learning Astro",
-        site: "https://morecat.netlify.app",
-        items: posts.map(post => ({
-            title: post.data.title,
-            pubDate: post.data.pubDate,
-            description: post.data.description,
-            link: `/posts/${post.slug}`
-        })),
-        customData: `<language>en-us</language>`,
-    });
+  const posts = await getCollection("posts");
+  return rss({
+    title: "Astro Lerner | Blog",
+    description: "My journey learning Astro",
+    site: "https://morecat.netlify.app",
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
 }
